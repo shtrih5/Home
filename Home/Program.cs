e99b7e1cbe33cd0b4782915d5ec1b7de8e8e0b1d
Юрманов Роман <shtrih@bk.ru>
2023-01-15 22:25:52 +0300

    Добавьте файлы проекта.

---
+++ Home/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ввидите количество элементов в массиве: \t");
+
+            int nummassive = Convert.ToInt32(Console.ReadLine());
+
+            int[] myArray;
+
+            myArray = new int[nummassive];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                Console.Write($"Ввидите значение для элемента № {i}:  ");
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
